cf6da7729405ac54e00fdadb5d66bdf16d8f012a
hajeongcheol <ha.jeongcheol@gmail.com>
2021-05-10 11:37:19 +0900

    delete

--- class/src/main/java/lecture/ClassRepository.java
+++
@@ -1,7 +0,0 @@
-package lecture;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ClassRepository extends PagingAndSortingRepository<Class, Long> {
-
-}
--- course/src/main/java/lecture/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package lecture;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
-
-}
